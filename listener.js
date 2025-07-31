be6904b4fc1cfbe9c12754ac95860a23b3486268
Mtsumi <mtsumijr@gmail.com>
2025-07-31 19:51:24 +0300

    removes bug

--- listener.js
+++ listener.js
@@ -26,7 +26,7 @@ async function handleOrderCreated(...args) {
     const res = await axios.post(`${process.env.FASTAPI_BASE_URL}/events/order-created`, payload, {
       headers: { "x-api-key": process.env.API_KEY }
     });
-    s
+
     console.log("✅ OrderCreated forwarded:", res.data);
   } catch (err) {
   if (err.response) {
